d6c7078d8704e39ba2a1fdb7e0a97fb42753549f
Luis <luishendrix92@gmail.com>
2015-11-02 20:20:26 -0800

    Create ayudas.cpp

---
+++ ayudas.cpp
@@ -0,0 +1,8 @@
+void gotoxy(int x,int y){  
+  HANDLE hcon;  
+  hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+  COORD dwPos;  
+  dwPos.X = x;  
+  dwPos.Y= y;  
+  SetConsoleCursorPosition(hcon,dwPos);  
+} // Fin de añadir GoToXY
