d4d8b74faf8d5556a94398cbd32db79ca5d5f9d3
Luis <luishendrix92@gmail.com>
2015-11-02 20:17:19 -0800

    Create blockbuster.cpp

---
+++ blockbuster.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include "ayudas.cpp"
+#include "render.cpp"
+
+using namespace std;
+
+int main() {
+	/*
+    1. Rentar Peliculas
+    2. Comprar Pelicula --> 
+    3. Membresias
+    4. Salir
+  */
+} // Fin del programa
