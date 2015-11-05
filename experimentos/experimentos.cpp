f3789ca4bfe561b0a36e44e3678c5e0d4d29220f
Luis <luishendrix92@gmail.com>
2015-11-05 13:57:49 -0800

    Experimental : Autocompletado
    Utilizando un ciclo infinito con Getch()

--- experimentos/experimentos.cpp
+++ experimentos/experimentos.cpp
@@ -11,5 +11,40 @@
 using namespace std;
 
 int main() {
-  //
-} // Fin del programa
+    vector<string> pelis;
+    size_t encontrado;
+    // Criterio de búsqueda
+    string criterio = "";
+    // Nuevas películas
+    pelis.push_back("Los simpson");
+    pelis.push_back("Los hermanos");
+    pelis.push_back("Con amor en el tren");
+    pelis.push_back("El amor es bello");
+
+    while (true) {
+      char tecla = getch();
+      if (tecla == 8) {
+        criterio.clear();
+        cout << "Backspace" << endl;
+      }
+      if (tecla != 8) criterio += tecla;
+      for (int i = 0; i < pelis.size(); i+=1) {
+        encontrado = pelis[i].find(criterio);
+        if (encontrado!=string::npos) {
+            cout << pelis[i] << endl;
+        }
+      }
+      cout << "-----------------------------" << endl;
+      cout << criterio << endl;
+      cout << "-----------------------------" << endl;
+    }
+    
+    /*cout << "Resultados de busqueda:" << endl;
+    cout << "-------------------------" << endl;
+    for (int i = 0; i < pelis.size(); i+=1) {
+        encontrado = pelis[i].find(criterio);
+        if (encontrado!=string::npos) {
+            cout << pelis[i] << endl;
+        }
+    }*/
+}
