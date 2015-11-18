acfbb367101bea57e393f1280f672456394b40ae
Luis <luishendrix92@gmail.com>
2015-11-18 13:26:29 -0800

    Lógica finalizada
    Lógica heredable para detectar teclas en cada menú finalizada, cambié la
    despedida por un alien en el espacio.

--- blockbuster.cpp
+++ blockbuster.cpp
@@ -19,24 +19,30 @@ int main() {
   system("cls");
   // Darle título a la ventana del programa
   SetConsoleTitle("Blockbuster: Rentas y Compras Online");
-  intro(280);
-  dibujarMenu("1_principal");
+  // Renderizar intro y menú principal
+  intro(280); dibujarMenu("1_principal");
 
-  while (tecla != 27) {
+  while (tecla != 27) { // Tecla NO es 'ESC'
     tecla = getch();
-    if(tecla == 0) {
-      tecla = getch();
-    } else {
+    if(tecla == 0) { tecla = getch(); } else {
       switch(tecla) {
         case 77: // Derecha
-          seleccionarBoton(2);
+          if (boton == 1) {
+            seleccionarBoton(2);
+          } else if (boton == 2) {
+            seleccionarBoton(1);
+          } // Fin de hacer toggle >
         break;
 
         case 75: // Izquierda
-          seleccionarBoton(1);
+          if (boton == 2) {
+            seleccionarBoton(1); boton = 1;
+          } else if (boton == 1) {
+            seleccionarBoton(2); boton = 2;
+          } // Fin de hacer toggle <
         break;
 
-        case 13: // ENTER
+        case 13: // 'ENTER'
           switch(boton) {
             case 1: menu::login();
             break;
--- menus.cpp
+++ menus.cpp
@@ -1,48 +1,3 @@
-void principal() {
-	//
-}
-//dar opcion crear cuenta o hacer login
-
-
-void registro() {
-	//
-}
-//pide nombre de usuario
-//pide contraseña
-//se validan los datos
-//pide contraseña de nuevo
-//se validan los datos
-//compara contraseña
-//se manda usuario y contraseña a archivo
-
-void login() {
-	//
-}
-//pide nombre usuario y contraseña
-//revisa columna manda a menu cliente o empleado
-
-
-void menuClientes() {
-  //
-}
-//credito
-//agregar credito
-
-void rentas() {
-  //
-}
-//rentar peliculas
-
-void membresias() {
-  //
-}
-//capturar datos de usuario
-
-void catalogo() {
-  //
-}
-//elegir categoria
-
 namespace menu {
   void empleados() {
     // modificar y agregar peliculas
@@ -55,4 +10,32 @@ namespace menu {
   void credito() {
     // modificar o agregar dinero a la cuenta
   }
-}
+
+  void catalogo() {
+    // elegir categoria
+  }
+
+  void principal() {
+    // dar opcion crear cuenta o hacer login
+  }
+
+  void registro() {
+    // pide usuario, clave 2 veces, valida y guarda
+  }
+
+  void login() {
+    // pide usuario y clave
+  }
+
+  void menuClientes() {
+    // elegir opciones de cliente
+  }
+
+  void rentas() {
+    // rentar películas
+  }
+
+  void membresias() {
+    // capturar datos de usuario
+  }
+} // Fin de namespace menu::función
