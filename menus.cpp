e9b5a99d0a74180cfe5f91985957dc399de76586
JesterRelik <ccyjl@outlook.com>
2015-11-17 20:31:52 -0800

    definicion de contenido
    primer borrador de estructura de menus

---
+++ menus.cpp
@@ -0,0 +1,44 @@
+void principal();
+//dar opcion crear cuenta o hacer login
+
+
+void registro();
+//pide nombre de usuario
+//pide contraseña
+//se validan los datos
+//pide contraseña de nuevo
+//se validan los datos
+//compara contraseña
+//se manda usuario y contraseña a archivo
+
+void login();
+//pide nombre usuario y contraseña
+//revisa columna manda a menu cliente o empleado
+
+
+void menuClientes();
+//credito
+//agregar credito
+
+
+void rentas();
+//qu
+
+void membresias();
+//capturar datos de usuario
+
+void catalogo();
+//elegir categoria
+
+void empleados();
+//modificar pelicula
+//agregar pelicula
+
+void comprar();
+//quitar pelicula
+//si peliculas igual a cero
+
+
+
+
+
